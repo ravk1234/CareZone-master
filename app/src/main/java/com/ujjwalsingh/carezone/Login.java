031e58ea903f736197ad9a43367bc5c3c972bd1d
ravk1234 <rkant4111@gmail.com>
2020-02-11 22:24:18 +0530

    Work Done - 1

---
+++ app/src/main/java/com/ujjwalsingh/carezone/Login.java
@@ -0,0 +1,87 @@
+package com.ujjwalsingh.carezone;
+
+//import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+
+    private EditText inputEmail, inputPassword;
+    private FirebaseAuth auth;
+    private Button btnSignup, btnLogin, btnReset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        auth = FirebaseAuth.getInstance();
+        inputEmail=findViewById(R.id.email);
+        inputPassword =findViewById(R.id.password);
+        btnLogin = findViewById(R.id.login);
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(Login.this, MainActivity.class));
+            finish();
+        }
+
+        setContentView(R.layout.activity_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString();
+                final String password = inputPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //progressBar.setVisibility(View.VISIBLE);
+
+                //authenticate user
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                //progressBar.setVisibility(View.GONE);
+
+                                        if (task.isSuccessful()) {
+                                            Intent intent = new Intent(Login.this,MainActivity.class);
+                                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                            startActivity(intent);
+                                            finish();
+                                        }
+                                        else{
+                                        Toast.makeText(Login.this, "Unable to Login with this email and password", Toast.LENGTH_SHORT).show();
+                                        }
+
+                            }
+
+                        });
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/ujjwalsingh/carezone/Signup.java
@@ -0,0 +1,104 @@
+package com.ujjwalsingh.carezone;
+
+//import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class Signup extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    Button button_register;
+    boolean val;
+    EditText fullname, email,password;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        mAuth = FirebaseAuth.getInstance();
+        fullname =findViewById(R.id.fullname);
+        email =findViewById(R.id.email);
+        password =findViewById(R.id.password);
+        button_register = findViewById(R.id.button_register);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        button_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (val) {
+                    String u_username = fullname.getText().toString();
+                    String u_email = email.getText().toString();
+                    String u_password = password.getText().toString();
+                    //String u_password = rpassword.getText().toString();
+                    if (TextUtils.isEmpty(u_username) || TextUtils.isEmpty(u_email) || TextUtils.isEmpty(u_password)) {
+                        Toast.makeText(Signup.this, "All credentials are required!", Toast.LENGTH_SHORT).show();
+                    }  else {
+                        register(u_username, u_email, u_password);
+                    }
+                }else
+                    button_register.setEnabled(false);
+            }
+        });
+    }
+
+    public void register(final String username, String email, String password){
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    FirebaseUser firebaseUser = mAuth.getCurrentUser();
+                    assert firebaseUser != null;
+                    String userUId = firebaseUser.getUid();
+                    reference = FirebaseDatabase.getInstance().getReference("Users").child(userUId);
+                    HashMap<String,String> hashMap = new HashMap<>();
+                    hashMap.put("id",userUId);
+                    hashMap.put("fullname",username);
+                    //hashMap.put("imgUrl","default");
+                    //hashMap.put("status","offline");
+                    hashMap.put("search",username.toLowerCase());
+
+                    reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(Signup.this,MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }
+                    });
+                }else{
+                    Toast.makeText(Signup.this, "Unable to Register with this username and password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+
+}
+
+
+
+
